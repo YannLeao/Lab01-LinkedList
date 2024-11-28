7771f1c16bdeead31c69fed55785ac936f17b4a9
YannLeao <yann.kjleao@ufrpe.br>
2024-11-28 09:59:46 -0300

    Entrega durante a aula

--- src/estruturaDeDados/Lista.java
+++ src/estruturaDeDados/Lista.java
@@ -2,49 +2,121 @@ package estruturaDeDados;
 
 public class Lista {
 
+    Node head;
+    int size;
+
+    public static class Node {
+        int valor;
+        Node prox;
+
+        Node(int valor) {
+            this.valor = valor;
+            this.prox = null;
+        }
+    }    
+
     public Lista() {
-        // TODO Auto-generated constructor stub
-        throw new UnsupportedOperationException("Unimplemented constructor 'ListaCircular'");
+        this.head = null;
+        this.size = 0;
     }
 
     public void insereInicio(int i) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'insereInicio'");
+        Node newNode = new Node(i);
+        newNode.prox = head;
+        head = newNode;
+        size++;
     }
 
     public boolean buscaElemento(int i) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'buscaElemento'");
+        if (head == null) {
+            return false;
+        }
+        
+        Node actual = head;
+        while(actual != null) {
+            if (actual.valor == i) {
+                return true;
+            }
+            actual = actual.prox;
+        }
+        return false;
     }
 
-    public Object buscaIndice(int i) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'buscaIndice'");
+    public Object buscaIndice(int index) {
+        Node actual = head;
+
+        for (int i = 0; i < index ; i++){
+            actual = actual.prox;
+        }
+        
+        return actual;
     }
 
     public void insereFim(int i) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'insereFim'");
+        Node newNode = new Node(i);
+        Node actual = head;
+
+        while(actual.prox != null) {
+            actual = actual.prox;
+        }
+        actual.prox = newNode;
+        size++;
     }
 
     public void removeInicio() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'removeInicio'");
+        if (size == 0) return;
+        head.prox = head;
+        size--;
     }
 
     public void removeFim() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'removeFim'");
+        if (size == 0) return;
+
+        Node actual = head;
+        while (actual.prox == null) {
+            actual = actual.prox;
+        }
+        actual = null;
+        size --;
     }
 
-    public void removeIndice(int i) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'removeIndice'");
+    public void removeIndice(int index) {
+        if (size < index) return;
+
+        Node actual = head;
+        for (int i = 0; i < index; i++) {
+            actual = actual.prox;
+        }
+        actual= null;
+        size --;
     }
 
-    public void insereElementoPosicao(int i, int j) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'insereElementoPosicao'");
+    public void insereElementoPosicao(int index, int value) {
+        
+        if (size == 0) {
+            insereInicio(value);
+        }
+        
+        Node actual = head;
+        Node ant = null;
+        
+        for (int i = 0; i < index; i++) {
+            actual = actual.prox;
+            for (int j = 0; j < index; j++) {
+                if (i == 0) {
+                    ant = head;
+                } else {
+                    ant = ant.prox;
+                }
+                
+            }
+            
+        }
+
+        Node newNode = new Node(value);
+        newNode.prox = actual;
+        ant.prox = newNode;        
+        
     }
     
 }
